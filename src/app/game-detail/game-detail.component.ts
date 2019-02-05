8e3401ef5a90dfbb04a5b215295d6add70a56f1e
Andredangad <andre.dang@hotmail.fr>
2019-02-05 12:06:34 +0100

    searchbar
    Premier test de la searchbar avec igdb

---
+++ src/app/game-detail/game-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+import { Jeu } from '../jeu';
+import { JeuService }  from '../jeu.service';
+
+@Component({
+  selector: 'app-game-detail',
+  templateUrl: './game-detail.component.html',
+  styleUrls: ['./game-detail.component.scss']
+})
+export class GameDetailComponent implements OnInit {
+	@Input() jeu: Jeu;
+
+  constructor(
+  private route: ActivatedRoute,
+  private jeuService: JeuService,
+  private location: Location
+) {}
+ngOnInit(){
+
+  const id = +this.route.snapshot.paramMap.get('id');
+  this.jeuService.getJeux(String(id))
+    .subscribe(jeu => this.jeu = jeu);
+
+}
+
+
+}
---
+++ src/app/jeu.ts
@@ -0,0 +1,8 @@
+export class Jeu {
+  id: number;
+  name: string;
+  url:string;
+  desc: string;
+  note: string;
+  img:string;
+}
